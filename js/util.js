ca769da1aadaa66b979edd66bc07a465755c8585
Jhonatan Wesley Camêlo <jwcmade@gmail.com>
2022-10-05 20:33:50 -0300

    mascaras js

---
+++ js/util.js
@@ -0,0 +1,10 @@
+jQuery(function ($) {
+
+  // Definições de Máscaras
+  $(".mascara-data").mask("99/99/9999");
+  $(".mascara-cpf").mask("999.999.999-99");
+  $(".mascara-cnpj").mask("99.999.999/9999-99");
+  $(".mascara-telefone").mask("(99)9999-9999?9");
+  $(".mascara-cep").mask("99.999-999");
+  $(".mascara-rg").mask("9.999.999");
+})
